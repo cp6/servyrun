1e66c601256faa30f00f1be40531e3345ae05666
cp6 <cp6@users.noreply.github.com>
2023-02-02 15:55:32 +1100

    Updated MySQL dump table

--- database/migrations/2023_01_27_013909_create_my_s_q_l_dumps_table.php
+++ database/migrations/2023_01_27_013909_create_my_s_q_l_dumps_table.php
@@ -9,22 +9,20 @@ return new class extends Migration {
     {
         Schema::create('mysql_dumps', function (Blueprint $table) {
             $table->char('id', 8);
-            $table->char('connection_id', 8);
+            $table->char('connection_id', 12);
             $table->char('server_id', 8);
             $table->char('database_id', 8);
             $table->char('db_connection_id', 8);
             $table->char('user_id', 6);
-            $table->char('certain_tables', 6)->default(null)->nullable();
+            $table->string('these_tables')->default(null)->nullable();
             $table->string('save_to')->default('')->nullable();
             $table->string('save_as');
             $table->boolean('compress')->default(0);
-            $table->boolean('quick')->default(0);
-            $table->boolean('opt')->default(0);
-            $table->boolean('lock_tables')->default(0);
-            $table->boolean('single_transaction')->default(0);
+            $table->tinyInteger('option')->default(0);
+            $table->string('flags')->default(null)->nullable();
             $table->dateTime('last_ran')->default(null)->nullable();
             $table->timestamps();
-            $table->unique(['user_id', 'database_id', 'certain_tables', 'save_as']);
+            $table->unique(['user_id', 'database_id', 'these_tables', 'save_as']);
             $table->foreign('connection_id')->references('id')->on('connections')->onDelete('cascade');
             $table->foreign('server_id')->references('id')->on('servers')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
