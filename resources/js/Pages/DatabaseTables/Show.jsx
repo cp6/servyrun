9aea7ed918bb1fc5802d9e6eca46c847c9ec3a8f
cp6 <cp6@users.noreply.github.com>
2023-02-02 11:44:08 +1100

    Updated table size_mb to have formatting

--- resources/js/Pages/DatabaseTables/Show.jsx
+++ resources/js/Pages/DatabaseTables/Show.jsx
@@ -110,7 +110,7 @@ export default function Show({auth, resource, tables, alert_type, alert_message}
                                         {
                                             (table.size_mb !== null) ?
                                                 <span
-                                                    className="ml-1 bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{table.size_mb} MB</span>
+                                                    className="ml-1 bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(table.size_mb)} MB</span>
                                                 : null
                                         }
                                     </li>)}
