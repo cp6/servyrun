ce4129cd24c483c7ad9253651869871728ae0a14
cp6 <cp6@users.noreply.github.com>
2023-02-03 12:03:45 +1100

    Added names for API routes Added middleware group for auth:api

--- routes/api.php
+++ routes/api.php
@@ -29,61 +29,66 @@ Route::get('/types', function () {
     return \App\Models\Type::all()->toJson();
 });
 
+Route::middleware('auth:api')->group(function () {
+//Everything in this group needs API bearer token
+
 //Databases
 //Refresh Database tables
-Route::get('/db/{database}/refresh', [DatabaseController::class, 'refresh'])->middleware(['auth:api', 'throttle:30,1'])->name('api.db.refresh');
+    Route::get('/db/{database}/refresh', [DatabaseController::class, 'refresh'])->middleware(['throttle:30,1'])->name('api.db.refresh');
 //Refresh Database table columns
-Route::get('/db/{database}/table/{databaseTable}/refresh', [DatabaseTableColumnController::class, 'refresh'])->middleware(['auth:api', 'throttle:30,1'])->name('api.db.table.refresh');
+    Route::get('/db/{database}/table/{databaseTable}/refresh', [DatabaseTableColumnController::class, 'refresh'])->middleware(['throttle:30,1'])->name('api.db.table.refresh');
 //Get Database table sizes and row counts
-Route::get('/db/{database}/size', [DatabaseController::class, 'doSize'])->middleware(['auth:api', 'throttle:30,1'])->name('api.db.table.size');
+    Route::get('/db/{database}/size', [DatabaseController::class, 'doSize'])->middleware(['throttle:30,1'])->name('api.db.table.size');
 
 
-Route::get('ping/{server}', [PingController::class, 'checkIsUp'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('ping/{server}', [PingController::class, 'checkIsUp'])->middleware(['throttle:30,1'])->name('ping.server');
 
-Route::get('dns/{domainName}/{type}', [ServerController::class, 'getIpForDomain'])->middleware(['auth:api', 'throttle:30,1'])->name('domain-for-ip');
+    Route::get('dns/{domainName}/{type}', [ServerController::class, 'getIpForDomain'])->middleware(['throttle:30,1'])->name('domain-for-ip');
 
-Route::get('connections/{connection}/run', [ConnectionController::class, 'run'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('connections/{connection}/run', [ConnectionController::class, 'run'])->middleware(['throttle:30,1'])->name('connection.run');
 
-Route::get('/user', [ApiController::class, 'userIndex'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/user', [ApiController::class, 'userIndex'])->middleware(['throttle:30,1'])->name('api.user.index');
 
-Route::get('/types', [ApiController::class, 'typesIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/types/{type}', [ApiController::class, 'typesShow'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/types', [ApiController::class, 'typesIndex'])->middleware(['throttle:30,1'])->name('api.types.index');
+    Route::get('/types/{type}', [ApiController::class, 'typesShow'])->middleware(['throttle:30,1'])->name('api.types.show');
 
-Route::get('/locations', [ApiController::class, 'locationsIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/locations/{location}', [ApiController::class, 'locationsShow'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/locations', [ApiController::class, 'locationsIndex'])->middleware(['throttle:30,1'])->name('api.locations.index');
+    Route::get('/locations/{location}', [ApiController::class, 'locationsShow'])->middleware(['throttle:30,1'])->name('api.locations.show');
 
-Route::get('/logs', [ApiController::class, 'actionLogsIndex'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/logs', [ApiController::class, 'actionLogsIndex'])->middleware(['throttle:30,1'])->name('api.logs.index');
 
-Route::get('/outputs', [ApiController::class, 'outputsIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/outputs/server/{server}', [ApiController::class, 'outputsServerIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/outputs/command/{command}', [ApiController::class, 'outputsCommandIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/outputs/{commandOutput}', [ApiController::class, 'outputsShow'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/outputs', [ApiController::class, 'outputsIndex'])->middleware(['throttle:30,1'])->name('api.outputs.index');
+    Route::get('/outputs/server/{server}', [ApiController::class, 'outputsServerIndex'])->middleware(['throttle:30,1'])->name('api.outputs.server.index');
+    Route::get('/outputs/command/{command}', [ApiController::class, 'outputsCommandIndex'])->middleware(['throttle:30,1'])->name('api.outputs.command.index');
+    Route::get('/outputs/{commandOutput}', [ApiController::class, 'outputsShow'])->middleware(['throttle:30,1'])->name('api.outputs.show');
 
-Route::get('/keys', [ApiController::class, 'keysIndex'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/keys', [ApiController::class, 'keysIndex'])->middleware(['throttle:30,1'])->name('api.keys.index');
 
-Route::get('/pings', [ApiController::class, 'pingsIndex'])->middleware(['auth:api', 'throttle:30,1']);
+    Route::get('/pings', [ApiController::class, 'pingsIndex'])->middleware(['throttle:30,1'])->name('api.pings.index');
 
-Route::get('/servers', [ApiController::class, 'serversIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/servers/{server}', [ApiController::class, 'serversShow'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/servers/{server}/ip', [ServerController::class, 'ip'])->middleware(['auth:api', 'throttle:20,1'])->name('server.ip');
-Route::patch('/servers/{server}', [ApiController::class, 'serversUpdate'])->middleware(['auth:api', 'throttle:20,1']);
-Route::post('/servers', [ApiController::class, 'serversStore'])->middleware(['auth:api', 'throttle:20,1']);
-Route::delete('/servers/{server}', [ApiController::class, 'serversDestroy'])->middleware(['auth:api', 'throttle:20,1']);
+    Route::get('/servers', [ApiController::class, 'serversIndex'])->middleware(['throttle:30,1'])->name('api.server.index');
+    Route::get('/servers/{server}', [ApiController::class, 'serversShow'])->middleware(['throttle:30,1'])->name('api.server.show');
+    Route::get('/servers/{server}/ip', [ServerController::class, 'ip'])->middleware(['throttle:20,1'])->name('server.ip');//IP for the server
+    Route::patch('/servers/{server}', [ApiController::class, 'serversUpdate'])->middleware(['throttle:20,1'])->name('api.server.update');
+    Route::post('/servers', [ApiController::class, 'serversStore'])->middleware(['throttle:20,1'])->name('api.server.store');
+    Route::delete('/servers/{server}', [ApiController::class, 'serversDestroy'])->middleware(['throttle:20,1'])->name('api.server.destroy');
 
-Route::get('/connections', [ApiController::class, 'connectionsIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/connections/{connection}', [ApiController::class, 'connectionsShow'])->middleware(['auth:api', 'throttle:30,1']);
-Route::patch('/connections/{connection}', [ApiController::class, 'connectionsUpdate'])->middleware(['auth:api', 'throttle:20,1']);
-Route::post('/connections', [ApiController::class, 'connectionsStore'])->middleware(['auth:api', 'throttle:20,1']);
-Route::delete('/connections/{connection}', [ApiController::class, 'connectionsDestroy'])->middleware(['auth:api', 'throttle:20,1']);
+    Route::get('/connections', [ApiController::class, 'connectionsIndex'])->middleware(['throttle:30,1'])->name('api.connection.index');
+    Route::get('/connections/{connection}', [ApiController::class, 'connectionsShow'])->middleware(['throttle:30,1'])->name('api.connection.show');
+    Route::patch('/connections/{connection}', [ApiController::class, 'connectionsUpdate'])->middleware(['throttle:20,1'])->name('api.connection.update');
+    Route::post('/connections', [ApiController::class, 'connectionsStore'])->middleware(['throttle:20,1'])->name('api.connection.store');
+    Route::delete('/connections/{connection}', [ApiController::class, 'connectionsDestroy'])->middleware(['throttle:20,1'])->name('api.connection.destroy');
 
-Route::get('/ips', [ApiController::class, 'ipsIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/ips/{ipAddress}', [ApiController::class, 'ipsShow'])->middleware(['auth:api', 'throttle:30,1']);
-Route::patch('/ips/{ipAddress}', [ApiController::class, 'ipsUpdate'])->middleware(['auth:api', 'throttle:20,1']);
-Route::post('/ips', [ApiController::class, 'ipsStore'])->middleware(['auth:api', 'throttle:20,1']);
-Route::delete('/ips/{ipAddress}', [ApiController::class, 'ipsDestroy'])->middleware(['auth:api', 'throttle:20,1']);
+    Route::get('/ips', [ApiController::class, 'ipsIndex'])->middleware(['throttle:30,1'])->name('api.ips.index');
+    Route::get('/ips/{ipAddress}', [ApiController::class, 'ipsShow'])->middleware(['throttle:30,1'])->name('api.ips.show');
+    Route::patch('/ips/{ipAddress}', [ApiController::class, 'ipsUpdate'])->middleware(['throttle:20,1'])->name('api.ips.update');
+    Route::post('/ips', [ApiController::class, 'ipsStore'])->middleware(['throttle:20,1'])->name('api.ips.store');
+    Route::delete('/ips/{ipAddress}', [ApiController::class, 'ipsDestroy'])->middleware(['throttle:20,1'])->name('api.ips.destroy');
 
-Route::get('/commands', [ApiController::class, 'commandsIndex'])->middleware(['auth:api', 'throttle:30,1']);
-Route::get('/commands/{command}', [ApiController::class, 'commandsShow'])->middleware(['auth:api', 'throttle:30,1']);
-Route::patch('/commands/{command}', [ApiController::class, 'commandsUpdate'])->middleware(['auth:api', 'throttle:20,1']);
-Route::post('/commands', [ApiController::class, 'commandsStore'])->middleware(['auth:api', 'throttle:20,1']);
-Route::delete('/commands/{command}', [ApiController::class, 'commandsDestroy'])->middleware(['auth:api', 'throttle:20,1']);
+    Route::get('/commands', [ApiController::class, 'commandsIndex'])->middleware(['throttle:30,1'])->name('api.commands.index');
+    Route::get('/commands/{command}', [ApiController::class, 'commandsShow'])->middleware(['throttle:30,1'])->name('api.commands.show');
+    Route::patch('/commands/{command}', [ApiController::class, 'commandsUpdate'])->middleware(['throttle:20,1'])->name('api.commands.update');
+    Route::post('/commands', [ApiController::class, 'commandsStore'])->middleware(['throttle:20,1'])->name('api.commands.store');
+    Route::delete('/commands/{command}', [ApiController::class, 'commandsDestroy'])->middleware(['throttle:20,1'])->name('api.commands.destroy');
+
+});
