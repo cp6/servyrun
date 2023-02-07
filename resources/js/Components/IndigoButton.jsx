5b141062543c172ec62d1aa2711a6e0b2fab1d8c
cp6 <cp6@users.noreply.github.com>
2023-02-07 15:30:58 +1100

    Updated button styling

--- resources/js/Components/IndigoButton.jsx
+++ resources/js/Components/IndigoButton.jsx
@@ -7,7 +7,7 @@ export default function IndigoButton({href, className = '', children, onClick}) 
             onClick={onClick}
             className={"text-white bg-indigo-700 hover:bg-indigo-800 dark:bg-indigo-600 dark:hover:bg-indigo-700 border border-transparent focus:ring-4 focus:ring-indigo-300 disabled:hover:bg-indigo-700 dark:focus:ring-indigo-900 dark:disabled:hover:bg-indigo-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center" + className}
         >
-                 <span class="flex items-center rounded-md">
+                 <span className="flex items-center rounded-md">
                      {children}
                  </span>
         </a>
--- resources/js/Components/TealButton.jsx
+++ resources/js/Components/TealButton.jsx
@@ -5,9 +5,9 @@ export default function TealButton({href, className = '', children, onClick}) {
         <a
             href={href}
             onClick={onClick}
-            className={"text-white bg-teal-600 hover:bg-teal-700 dark:bg-teal-600 dark:hover:bg-teal-700 border border-transparent focus:ring-4 focus:ring-teal-300 disabled:hover:bg-teal-700 dark:focus:ring-teal-900 dark:disabled:hover:bg-teal-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center" + className}
+            className={"text-white bg-teal-500 hover:bg-teal-700 dark:bg-teal-500 dark:hover:bg-teal-700 border border-transparent focus:ring-4 focus:ring-teal-300 disabled:hover:bg-teal-700 dark:focus:ring-teal-900 dark:disabled:hover:bg-teal-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center" + className}
         >
-                 <span class="flex items-center rounded-md">
+                 <span className="flex items-center rounded-md">
                      {children}
                  </span>
         </a>
