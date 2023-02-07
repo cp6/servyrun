37f4f1c6020633cc870a7e20a93ff0c47ecc0190
cp6 <cp6@users.noreply.github.com>
2023-02-08 10:51:18 +1100

    Added timeout integer column to command groups table

--- database/migrations/2023_02_07_130933_create_command_groups_table.php
+++ database/migrations/2023_02_07_130933_create_command_groups_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('title')->default(null)->nullable();
             $table->integer('server_count')->default(null)->nullable();
             $table->boolean('email_output')->default(1);
+            $table->integer('timeout')->default(10);
             $table->timestamps();
             $table->foreign('command_id')->references('id')->on('commands')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
