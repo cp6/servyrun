f1b524b3120d18859f1fdef1613923ed5b5940fc
cp6 <cp6@users.noreply.github.com>
2023-02-08 15:26:51 +1100

    Updated button components to have disabled state

--- resources/js/Components/EmeraldButton.jsx
+++ resources/js/Components/EmeraldButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function EmeraldButton({href, className = '', children, onClick}) {
+export default function EmeraldButton({href, className = '', children, onClick, disabled}) {
     return (
         <a
             href={href}
             onClick={onClick}
-            className={"text-white bg-emerald-600 hover:bg-emerald-700 dark:bg-emerald-600 dark:hover:bg-emerald-700 border border-transparent focus:ring-4 focus:ring-emerald-300 disabled:hover:bg-emerald-700 dark:focus:ring-emerald-900 dark:disabled:hover:bg-emerald-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center" + className}
+            className={`text-white bg-emerald-600 hover:bg-emerald-700 dark:bg-emerald-600 dark:hover:bg-emerald-700 border border-transparent focus:ring-4 focus:ring-emerald-300 disabled:hover:bg-emerald-700 dark:focus:ring-emerald-900 dark:disabled:hover:bg-emerald-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center hover:cursor-pointer ${(disabled) ? "pointer-events-none cursor-progress" : ""}` + className}
         >
                  <span className="flex items-center rounded-md">
                      {children}
--- resources/js/Components/TealButton.jsx
+++ resources/js/Components/TealButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function TealButton({href, className = '', children, onClick}) {
+export default function TealButton({href, className = '', children, onClick, disabled}) {
     return (
         <a
             href={href}
             onClick={onClick}
-            className={"text-white bg-teal-500 hover:bg-teal-700 dark:bg-teal-500 dark:hover:bg-teal-700 border border-transparent focus:ring-4 focus:ring-teal-300 disabled:hover:bg-teal-700 dark:focus:ring-teal-900 dark:disabled:hover:bg-teal-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center" + className}
+            className={`text-white bg-teal-500 hover:bg-teal-700 dark:bg-teal-500 dark:hover:bg-teal-700 border border-transparent focus:ring-4 focus:ring-teal-300 disabled:hover:bg-teal-700 dark:focus:ring-teal-900 dark:disabled:hover:bg-teal-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-sm focus:z-10 inline-flex items-center rounded-lg px-5 py-2 text-center hover:cursor-pointer ${(disabled) ? "pointer-events-none cursor-progress" : ""}` + className}
         >
                  <span className="flex items-center rounded-md">
                      {children}
