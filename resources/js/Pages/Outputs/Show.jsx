27051c1a52987fa85860c23f7c0524542bd5fccc
cp6 <cp6@users.noreply.github.com>
2023-02-10 14:10:35 +1100

    Added server button to command output show page

--- resources/js/Pages/Outputs/Show.jsx
+++ resources/js/Pages/Outputs/Show.jsx
@@ -3,6 +3,8 @@ import {Head, usePage} from '@inertiajs/inertia-react';
 import React from "react";
 import {Card} from "flowbite-react";
 import Output from "@/Components/Output";
+import EmeraldButton from "@/Components/EmeraldButton";
+import {HiCpuChip} from "react-icons/all";
 
 export default function Show({auth, resource}) {
     const user = usePage().props.auth.user;
@@ -14,11 +16,12 @@ export default function Show({auth, resource}) {
             <Head title={"Command " + resource.id}/>
             <div className="py-6 px-2 mx-auto max-w-7xl lg:py-8">
                 <div className="flex flex-wrap gap-2 mb-4">
+                    <EmeraldButton href={route('server.show', resource.server.id)}><HiCpuChip className="mr-2 h-5 w-5"/>Server</EmeraldButton>
                 </div>
                 <div className="grid gap-2 grid-cols-1 sm:gap-4">
-                    <Card>
+                    <Card className={'dark:bg-gray-700'}>
                         <Output title={resource.server.hostname} the_command={resource.the_command}
-                                created_at={resource.created_at} the_output={resource.output} seconds={resource.seconds_taken}></Output>
+                                created_at={resource.created_at} the_output={resource.output} seconds={resource.seconds_taken} rows={22}></Output>
                     </Card>
                 </div>
             </div>
