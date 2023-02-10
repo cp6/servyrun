25cf3b9d73230a27aefdc807a7f760e96e73949a
cp6 <cp6@users.noreply.github.com>
2023-02-11 01:20:19 +1100

    Minimized switcher JS

--- resources/js/switcher.js
+++ resources/js/switcher.js
@@ -1,9 +1,1 @@
-if (
-    localStorage.getItem('color-theme') === 'dark' ||
-    (!('color-theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-) {
-    document.documentElement.classList.add('dark');
-} else {
-    document.documentElement.classList.remove('dark');
-}
+"dark"!==localStorage.getItem("color-theme")&&("color-theme"in localStorage||!window.matchMedia("(prefers-color-scheme: dark)").matches)?document.documentElement.classList.remove("dark"):document.documentElement.classList.add("dark");
