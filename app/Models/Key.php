104fbb7e124dfc65f290617b1ab8ec301f1e837e
cp6 <cp6@users.noreply.github.com>
2023-02-11 01:20:49 +1100

    Added delete key file when  deleting key.

--- app/Models/Key.php
+++ app/Models/Key.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Models\Scopes\UserOwnedScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Key extends Model
 {
@@ -29,11 +30,16 @@ class Key extends Model
         });
 
         static::created(function (Key $key) {
-            ActionLog::make(1, 'create', 'key', 'Created key '.$key->id);
+            ActionLog::make(1, 'create', 'key', 'Created key ' . $key->id);
         });
 
         static::updated(function (Key $key) {
-            ActionLog::make(1, 'update', 'key', 'Updated key '.$key->id);
+            ActionLog::make(1, 'update', 'key', 'Updated key ' . $key->id);
+        });
+
+        static::deleting(function (Key $key) {
+            $delete = Storage::disk('private')->delete('keys/' . $key->saved_as);
+            ActionLog::make(1, 'delete', 'key', 'Attempted to delete key, result:' . ($delete) ? 'success' : 'failed');
         });
 
         static::deleted(function (Key $key) {
