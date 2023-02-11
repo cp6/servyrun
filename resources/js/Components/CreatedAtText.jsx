a56f9cf849d45c2c23c7dce5b3e6cdaa547cf237
cp6 <cp6@users.noreply.github.com>
2023-02-11 14:07:58 +1100

    Added pre text for CreateAtText component

--- resources/js/Components/CreatedAtText.jsx
+++ resources/js/Components/CreatedAtText.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {format} from "date-fns";
 
-export default function CreatedAtText({created_at, string_format = 'hh:mma do LLL yyyy'}) {
+export default function CreatedAtText({created_at, string_format = 'hh:mma do LLL yyyy', pre_text = 'Created:'}) {
 
     let formatted_created_at_date = format(new Date(created_at), string_format);
 
     return (
         <p className="my-4 text-md leading-none text-gray-900 dark:text-white"><span
-            className='font-light'>Created:</span> {formatted_created_at_date}</p>
+            className='font-light'>{pre_text}</span> {formatted_created_at_date}</p>
     );
 }
