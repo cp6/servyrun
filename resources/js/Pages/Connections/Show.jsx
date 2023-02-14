54e3f9d13b5a61c5a9160a828c61dff8a709d9c2
cp6 <cp6@users.noreply.github.com>
2023-02-14 12:02:09 +1100

    Added timer to script execute (connection show)

--- resources/js/Pages/Connections/Show.jsx
+++ resources/js/Pages/Connections/Show.jsx
@@ -1,7 +1,7 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head, useForm} from '@inertiajs/inertia-react';
 import {Button, Modal, Select} from "flowbite-react";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import InputLabel from "@/Components/InputLabel";
 import TextInput from "@/Components/TextInput";
 import InputError from "@/Components/InputError";
@@ -10,11 +10,14 @@ import ReactDOM from "react-dom/client";
 import Output from "@/Components/Output";
 import ResponseAlert from "@/Components/Alert";
 import BackButton from "@/Components/BackButton";
-import {HiPencil, HiServer, HiTrash} from "react-icons/hi";
+import {HiPencil, HiQuestionMarkCircle, HiServer, HiTrash} from "react-icons/hi";
 
 export default function Show({auth, resource, ip, method, commands, alert_type, alert_message}) {
 
     const [isEmailChecked, setIsEmailChecked] = useState(false);
+
+    const [runTime, setRunTime] = useState(0);
+    const [running, setRunning] = useState(false);
 
     const handleEmailChange = event => {
         setIsEmailChecked(event.target.checked);
@@ -28,12 +31,26 @@ export default function Show({auth, resource, ip, method, commands, alert_type, 
         email: ''
     });
 
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() => {
+                setRunTime((prevTime) => prevTime + 10);
+            }, 10);
+        } else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [running]);
+
+
     const [showModal, setShowModal] = useState(false);
 
     const [hasAlert, setHasAlert] = React.useState(true);
 
     async function postData() {
-
+        setRunTime(0);
+        setRunning(true);
         const response = await fetch(route('connection.run', resource.id), {
             method: 'POST',
             headers: {
@@ -48,6 +65,8 @@ export default function Show({auth, resource, ip, method, commands, alert_type, 
                 'email': data.email || false
             })
         });
+
+        setRunning(false);
 
         return response.json();
     }
@@ -114,10 +133,18 @@ export default function Show({auth, resource, ip, method, commands, alert_type, 
                                 <HiTrash
                                     className="mr-2 h-6 w-6 text-gray-600 dark:text-white hover:text-gray-700 hover:dark:text-gray-300 inline hover:cursor-pointer"
                                     onClick={() => setShowModal(true)} title={'Delete connection'}/>
-                                <HiPencil className="md:ml-2 ml-1 h-6 w-6 text-gray-600 dark:text-white inline hover:cursor-pointer"
-                                          onClick={event => window.location.href = route('connection.edit', resource.id)} title={'Edit connection'}/>
-                                <HiServer className="md:ml-3 ml-2 h-6 w-6 text-gray-600 dark:text-white inline hover:cursor-pointer"
-                                          onClick={event => window.location.href = route('server.show', resource.server.id)} title={'Go to server'}/>
+                                <HiPencil
+                                    className="md:ml-2 ml-1 h-6 w-6 text-gray-600 dark:text-white inline hover:cursor-pointer"
+                                    onClick={event => window.location.href = route('connection.edit', resource.id)}
+                                    title={'Edit connection'}/>
+                                <HiServer
+                                    className="md:ml-3 ml-2 h-6 w-6 text-gray-600 dark:text-white inline hover:cursor-pointer"
+                                    onClick={event => window.location.href = route('server.show', resource.server.id)}
+                                    title={'Go to server'}/>
+                                <HiQuestionMarkCircle
+                                    className="md:ml-3 ml-2 h-6 w-6 text-gray-600 dark:text-white inline hover:cursor-pointer"
+                                    onClick={event => window.location.href = route('connection.debug', resource.id)}
+                                    title={'Debug connection'}/>
                             </small>
                         </div>
                         <form onSubmit={submit}>
@@ -187,8 +214,13 @@ export default function Show({auth, resource, ip, method, commands, alert_type, 
                     <p className='pl-4 text-gray-700 dark:text-gray-300'><a
                         href={route('outputs.show.server', resource.server.id)}>View all</a>
                     </p>
-                    <div className="py-6 px-4 mx-auto max-w-7xl lg:py-8" id="command_output_div">
-                        <span className="text-gray-400 dark:text-gray-500">Nothing run yet</span>
+                    <div className="pt-2 px-4 mx-auto max-w-7xl">
+                        {
+                            (running) ? <p className={'text-gray-600 dark:text-gray-300 mb-2'}><code className={'text-red-500 bg-gray-300 dark:bg-black p-1 rounded-md my-2'}>{data.the_command1 ?? null}</code> {(runTime / 1000) % 60}s</p> : null
+                        }
+                    </div>
+                    <div className="py-4 px-4 mx-auto max-w-7xl" id="command_output_div">
+                        <span className="text-gray-400 dark:text-gray-500">No output yet</span>
                     </div>
                 </section>
             </div>
