df07b20b9ad0aba4cfa5755f516a7d3d94de04e1
cp6 <cp6@users.noreply.github.com>
2023-02-16 15:56:52 +1100

    Added className parameter for ResourceEmptyText.jsx

--- resources/js/Components/ResourceEmptyText.jsx
+++ resources/js/Components/ResourceEmptyText.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export default function ResourceEmptyText({resource}) {
+export default function ResourceEmptyText({resource, className}) {
     return (
-        <h2 className={'text-md my-1 font-semibold text-gray-600 md:text-lg dark:text-gray-300'}>No {resource} currently</h2>
+        <h2 className={'text-md my-1 font-semibold text-gray-600 md:text-lg dark:text-gray-300 ' + className}>No {resource} currently</h2>
     );
 }
