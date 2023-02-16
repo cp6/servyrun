3060a247a8473d53f96fb57773db92ff859d7f54
cp6 <cp6@users.noreply.github.com>
2023-02-16 13:46:46 +1100

    Updated MySQlDumps index to no longer use cards

--- resources/js/Pages/MySQLDumps/Index.jsx
+++ resources/js/Pages/MySQLDumps/Index.jsx
@@ -1,7 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import {Head} from '@inertiajs/inertia-react';
 import React from "react";
-import {Card} from "flowbite-react";
 import ResourceEmptyText from "@/Components/ResourceEmptyText";
 import ResponseAlert from "@/Components/Alert";
 import AddButton from "@/Components/AddButton";
@@ -24,17 +23,24 @@ export default function Index({auth, dumps, alert_type, alert_message}) {
                                alert_message={alert_message}></ResponseAlert>
                 <div className="grid gap-2 grid-cols-1 sm:grid-cols-4 sm:gap-4">
                     {dumps.length > 0 ?
-                        dumps.map(databases =>
-                            <Card key={databases.id} href={route('mysqldump.show', databases.id)}
-                                  className={'dark:bg-gray-700 hover:dark:bg-gray-900'}>
-                                <div className="flex flex-col items-center pb-3">
-                                    <h2 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                                        {databases.database.name}
-                                    </h2>
-                                    <span
-                                        className="text-sm text-gray-500 dark:text-gray-400">{databases.database_conn.username}@{databases.database_conn.host}</span>
+                        dumps.map(dump =>
+                            <section key={dump.id}
+                                     className="bg-white/50 dark:bg-gray-700 rounded-lg shadow-sm hover:cursor-pointer"
+                                     onClick={event => window.location.href = route('mysqldump.show', servers.id)}>
+                                <div className="md:py-2 py-4 px-2 mx-auto max-w-6xl">
+                                    <div className="flex items-center justify-between mb-3">
+                                        <div></div>
+                                        <small className="text-end"></small>
+                                    </div>
+                                    <div className="flex flex-col items-center pb-3">
+                                        <h2 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                                            {dump.database.name}
+                                        </h2>
+                                        <span
+                                            className="text-sm text-gray-500 dark:text-gray-400">{dump.database_conn.username}@{dump.database_conn.host}</span>
+                                    </div>
                                 </div>
-                            </Card>
+                            </section>
                         )
                         :
                         <ResourceEmptyText resource={'MySQL dumps'}></ResourceEmptyText>
