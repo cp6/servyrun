593dd434a8444fad86d0d2dfcaa10b7d34a6e282
cp6 <cp6@users.noreply.github.com>
2023-02-27 13:10:32 +1100

    Updated dark and light theme switcher to work on one click and to use a state

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -6,47 +6,34 @@ import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import {Link} from '@inertiajs/inertia-react';
 
 export default function Authenticated({auth, header, children}) {
+
+    const [isDarkTheme, setIsDarkTheme] = useState(document.documentElement.classList.contains('dark'));
+
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
-    function processThemeChange(themeToggleDarkIcon, themeToggleLightIcon) {
-
-        if (localStorage.getItem('color-theme') && localStorage.getItem('color-theme') === 'light') {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
-        } else if (localStorage.getItem('color-theme') && localStorage.getItem('color-theme') === 'dark') {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
-        } else if (document.documentElement.classList.contains('dark')) {
-            document.documentElement.classList.remove('dark');
-            localStorage.setItem('color-theme', 'light');
+    function processThemeChange() {
+        if (localStorage.getItem('color-theme')) {
+            if (localStorage.getItem('color-theme') === 'light') {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            }
         } else {
-            document.documentElement.classList.add('dark');
-            localStorage.setItem('color-theme', 'dark');
+            if (document.documentElement.classList.contains('dark')) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('color-theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('color-theme', 'dark');
+            }
         }
-        themeToggleDarkIcon.classList.toggle('hidden');
-        themeToggleLightIcon.classList.toggle('hidden');
     }
 
     function switchTheme() {
-        var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-        var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            themeToggleLightIcon.classList.remove('hidden');
-        } else {
-            themeToggleDarkIcon.classList.remove('hidden');
-        }
-
-        var themeToggleBtn = document.getElementById('theme-toggle');
-        var themeToggleBtnMobile = document.getElementById('theme-toggle-mobile');
-
-        themeToggleBtn.addEventListener('click', function () {
-            processThemeChange(themeToggleDarkIcon, themeToggleLightIcon);
-        });
-
-        themeToggleBtnMobile.addEventListener('click', function () {
-            processThemeChange(themeToggleDarkIcon, themeToggleLightIcon);
-        });
+        setIsDarkTheme(isDarkTheme => !isDarkTheme);
+        processThemeChange();
     }
 
     return (
@@ -96,12 +83,14 @@ export default function Authenticated({auth, header, children}) {
                                 </NavLink>
                                 <button id="theme-toggle" onClick={switchTheme} type="button"
                                         className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 themeToggle">
-                                    <svg id="theme-toggle-dark-icon" className="w-5 h-5 hidden" fill="currentColor"
+                                    <svg id="theme-toggle-dark-icon"
+                                         className={(isDarkTheme) ? "w-5 h-5 hidden" : "w-5 h-5"} fill="currentColor"
                                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                                     </svg>
-                                    <svg id="theme-toggle-light-icon" className="w-5 h-5" fill="currentColor"
+                                    <svg id="theme-toggle-light-icon"
+                                         className={(isDarkTheme) ? "w-5 h-5" : "w-5 h-5 hidden"} fill="currentColor"
                                          viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                         <path
                                             d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
@@ -207,12 +196,14 @@ export default function Authenticated({auth, header, children}) {
                         </ResponsiveNavLink>
                         <button id="theme-toggle-mobile" onClick={switchTheme} type="button"
                                 className="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 themeToggle">
-                            <svg id="theme-toggle-dark-icon" className="w-5 h-5 hidden" fill="currentColor"
+                            <svg id="theme-toggle-dark-icon" className={(isDarkTheme) ? "w-5 h-5 hidden" : "w-5 h-5"}
+                                 fill="currentColor"
                                  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                             </svg>
-                            <svg id="theme-toggle-light-icon" className="w-5 h-5" fill="currentColor"
+                            <svg id="theme-toggle-light-icon" className={(isDarkTheme) ? "w-5 h-5" : "w-5 h-5 hidden"}
+                                 fill="currentColor"
                                  viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
--- resources/js/switcher.js
+++ resources/js/switcher.js
@@ -1,1 +1,1 @@
-"dark"!==localStorage.getItem("color-theme")&&("color-theme"in localStorage||!window.matchMedia("(prefers-color-scheme: dark)").matches)?document.documentElement.classList.remove("dark"):document.documentElement.classList.add("dark");
+"dark"!==localStorage.getItem("color-theme")&&("color-theme"in localStorage||!window.matchMedia("(prefers-color-scheme: dark)").matches)?(document.documentElement.classList.remove("dark"),document.documentElement.classList.add("light")):(document.documentElement.classList.add("dark"),document.documentElement.classList.remove("light"));
