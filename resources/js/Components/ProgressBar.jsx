57c026ecbaaee30e42f2a60c642ddd880fc11ed4
cp6 <cp6@users.noreply.github.com>
2023-03-06 11:52:05 +1100

    Added margin to empty Progressbar so page doesnt bounce when showing and removing the progress bar

--- resources/js/Components/ProgressBar.jsx
+++ resources/js/Components/ProgressBar.jsx
@@ -4,12 +4,16 @@ export default function ProgressBar({value}) {
     if (value !== null) {
         return (
             <div className="w-full mt-4">
-                <div className="shadow w-full bg-grey-light">
-                    <div className="bg-blue-500 text-sm leading-none py-1 text-center text-white rounded-sm"
+                <div className="shadow w-full bg-gray-200 dark:bg-gray-500">
+                    <div className={`bg-blue-500 text-sm leading-none py-1 text-center text-white rounded-sm`}
                          style={{width: value + '%'}}>{value}%
                     </div>
                 </div>
             </div>
+        );
+    } else {
+        return (
+            <div className="w-full my-9"></div>
         );
     }
 }
