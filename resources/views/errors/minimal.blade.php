722efdfcf3ced2a9920f9ca08dfe7f2335a0ff02
cp6 <cp6@users.noreply.github.com>
2023-03-07 11:03:05 +1100

    Updated error view layout

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -7,16 +7,15 @@
     @vite(['resources/css/app.css', 'resources/css/app.js', 'resources/js/switcher.js'])
 </head>
 <body class="antialiased bg-gray-50 dark:bg-gray-800">
-<section>
-    <div class="py-10 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center">
-            <h1 class="mb-4 text-6xl font-bold lg:text-7xl text-gray-800 dark:text-white">@yield('code')</h1>
-            <p class="mb-6 text-2xl tracking-tight font-bold text-gray-700 md:text-3xl dark:text-gray-200">@yield('message')</p>
-            <a href="{{url()->previous()}}"
-               class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-6 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go back</a>
-            <a href="{{route('dashboard')}}"
-               class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-6 py-2 text-center dark:focus:ring-primary-900 my-4">Home</a>
-        </div>
+<section class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-800">
+    <div class="px-4 mx-auto max-w-screen-xl lg:px-6 text-center">
+        <h1 class="mb-4 text-6xl font-bold lg:text-7xl text-gray-800 dark:text-white">@yield('code')</h1>
+        <p class="mb-6 text-2xl tracking-tight font-bold text-gray-700 md:text-3xl dark:text-gray-200">@yield('message')</p>
+        <a href="{{url()->previous()}}"
+           class="inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm px-7 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go
+            back</a>
+        <a href="{{route('dashboard')}}"
+           class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-md text-sm px-6 py-2 text-center dark:focus:ring-primary-900 my-4">Home</a>
     </div>
 </section>
 </body>
