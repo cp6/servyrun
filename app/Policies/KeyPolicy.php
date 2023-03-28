21c2467885a83892c64194028f181a2897eb3f5c
cp6 <cp6@users.noreply.github.com>
2023-03-28 23:52:55 +1100

    Cleaned up Policy files

--- app/Policies/KeyPolicy.php
+++ app/Policies/KeyPolicy.php
@@ -10,59 +10,16 @@ class KeyPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Key  $key
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function view(User $user, Key $key)
     {
         return $user->id === $key->user_id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Key  $key
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function update(User $user, Key $key)
     {
         return $user->id === $key->user_id;
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Key  $key
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function delete(User $user, Key $key)
     {
         return $user->id === $key->user_id;
@@ -73,25 +30,11 @@ class KeyPolicy
         return $user->id === $key->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Key  $key
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function restore(User $user, Key $key)
     {
         return $user->id === $key->user_id;
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Key  $key
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
     public function forceDelete(User $user, Key $key)
     {
         return $user->id === $key->user_id;
