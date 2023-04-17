06502535992e55931e6d80bea000286b49d6dfec
cp6 <cp6@users.noreply.github.com>
2023-04-17 23:51:02 +1000

    updated seeder to only seed user if environment is local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,19 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call(UserSeeder::class);
+        if (App::environment('local')) {
+            $this->call(UserSeeder::class);
+            //$this->call(ServerSeeder::class);
+            //$this->call(IpAddressSeeder::class);
+            //$this->call(ConnectionSeeder::class);
+        }
         $this->call(TypeSeeder::class);
         $this->call(LocationSeeder::class);
-        //$this->call(ServerSeeder::class);
-        //$this->call(IpAddressSeeder::class);
-        //$this->call(ConnectionSeeder::class);
     }
 }
