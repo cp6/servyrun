7b6abc89db25deaba146a3d5322016f083689122
cp6 <cp6@users.noreply.github.com>
2023-04-19 01:01:44 +1000

    Updated navbar link hover text color and border

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -6,8 +6,8 @@ export default function NavLink({ href, active, children }) {
             href={href}
             className={
                 active
-                    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 dark:text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-700 dark:text-gray-200 hover:text-gray-700 hover:dark:text-gray-400 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+                    ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 dark:text-white focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+                    : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-700 dark:text-gray-200 hover:text-gray-800 hover:dark:text-gray-400 hover:border-indigo-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
             }
         >
             {children}
