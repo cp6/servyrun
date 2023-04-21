29e035f2833f136876c1a94dfaac1ad1dba3457b
cp6 <cp6@users.noreply.github.com>
2023-04-21 10:28:08 +1000

    Added files for Docker

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Run setup only if .env file doesn't exist.
+if [ ! -e .env.production ]
+then
+cat > .env.production << EOF
+APP_NAME=Surcuri
+APP_DEBUG=false
+APP_KEY=
+
+DB_CONNECTION=mysql
+DB_HOST=${DB_HOST}
+DB_DATABASE=${DB_DATABASE}
+DB_USERNAME=${DB_USERNAME}
+DB_PASSWORD=${DB_PASSWORD}
+APP_URL=${APP_URL}
+EOF
+php artisan key:generate --no-interaction --force
+fi
+
+# php artisan migrate:fresh --seed
+php artisan serve --host=0.0.0.0 --port=8000 --env=production
