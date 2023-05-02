99f1e400b629810927df7772d7dc514bea17d3a2
cp6 <cp6@users.noreply.github.com>
2023-05-02 14:28:01 +1000

    Fixed error throwing when $account_ip->login_ip_only is null I.E no accounts found

--- app/Http/Middleware/AllowedIpMiddleware.php
+++ app/Http/Middleware/AllowedIpMiddleware.php
@@ -12,7 +12,7 @@ class AllowedIpMiddleware
     {//Allow only whitelisted IP for account to login
         $account_ip = User::where('email', $request->email)->select('login_ip_only')->first();
 
-        if (!is_null($account_ip->login_ip_only) && $request->ip() !== $account_ip->login_ip_only) {
+        if (isset($account_ip->login_ip_only) && !is_null($account_ip->login_ip_only) && $request->ip() !== $account_ip->login_ip_only) {
             abort(401);
         }
 
