2d204449270a486473c49361c4dad6d030ba20f9
cp6 <cp6@users.noreply.github.com>
2023-05-08 15:28:22 +1000

    Added build

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@
  * building robust, powerful web applications using React + Laravel.
  */
 
-import './bootstrap';
+//import './bootstrap.js';
 
 /**
  * Next, we will create a fresh React component instance and attach it to
