a187046f6ea4b47c486dd372773ba04c0385019d
cp6 <cp6@users.noreply.github.com>
2023-05-12 15:59:16 +1000

    Updated Connection.php fillable

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -19,7 +19,7 @@ class Connection extends Model
 
     protected $keyType = 'string';
 
-    protected $fillable = ['type', 'username', 'ssh_port', 'key_id', 'password', 'submit_password', 'sudo', 'last_used', 'status'];
+    protected $fillable = ['server_id', 'user_id', 'type', 'username', 'ssh_port', 'key_id', 'password', 'submit_password', 'sudo', 'last_used'];
 
     protected static function boot(): void
     {
