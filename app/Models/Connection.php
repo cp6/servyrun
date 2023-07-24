eb6b704312e37789bbfd9c97f0a8521b7792a9c9
cp6 <cp6@users.noreply.github.com>
2023-07-24 22:22:10 +1000

    Created getUptime() function to get uptime in seconds

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -151,6 +151,12 @@ class Connection extends Model
         return $ssh;
     }
 
+    public static function getUptime(SSH2 $connection): int
+    {
+        $command = "awk '{print $1}' /proc/uptime";
+        return $connection->exec($command);
+    }
+
     public static function getCpuUsedPercent(SSH2 $connection): float
     {
         $command = "cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}'";
