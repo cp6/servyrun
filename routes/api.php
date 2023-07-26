fc7f38482bdddfe3a20ea7669f6d28200f2f2788
cp6 <cp6@users.noreply.github.com>
2023-07-26 22:13:18 +1000

    Create an ALL server usage API route

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,7 @@ Route::middleware(['auth:api', 'allowedIpApi'])->group(function () {
     Route::get('/servers/{server}/pings', [ApiController::class, 'serversPings'])->name('api.server.pings');
     Route::get('/servers/{server}/commands', [ApiController::class, 'serversCommands'])->name('api.server.commands');
     Route::get('/servers/{server}/connections', [ApiController::class, 'serversConnections'])->name('api.server.connections');
+    Route::get('/servers/{server}/usage/all', [ApiController::class, 'serversUsageAll'])->name('api.server.usage.all');
     Route::get('/servers/{server}/usage/cpu', [ApiController::class, 'serversUsageCpu'])->name('api.server.usage.cpu');
     Route::get('/servers/{server}/usage/ram', [ApiController::class, 'serversUsageRam'])->name('api.server.usage.ram');
     Route::get('/servers/{server}/usage/disk', [ApiController::class, 'serversUsageDisk'])->name('api.server.usage.disk');
