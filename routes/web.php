41c7b6ca0776da48f9aef36bcc42940f2a020a68
cp6 <cp6@users.noreply.github.com>
2023-07-28 18:40:08 +1000

    Added routes for usage charts

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/servers/{server}', [ServerController::class, 'show'])->name('server.show');
     Route::get('/servers/{server}/get-information', [ServerController::class, 'getInformation'])->name('server.get-information');
     Route::get('/servers/{server}/usage', [ServerController::class, 'getUsage'])->name('server.usage');
+    Route::get('/servers/{server}/usage/ram', [ServerController::class, 'ramUsage'])->name('server.usage.ram');
+    Route::get('/servers/{server}/usage/cpu', [ServerController::class, 'cpuUsage'])->name('server.usage.cpu');
+    Route::get('/servers/{server}/usage/disk', [ServerController::class, 'diskUsage'])->name('server.usage.disk');
     Route::get('/servers/{server}/uptime', [ServerController::class, 'getUptime'])->name('server.uptime');
     Route::get('/servers/{server}/edit', [ServerController::class, 'edit'])->name('server.edit');
     Route::patch('/servers/{server}', [ServerController::class, 'update'])->name('server.update');
