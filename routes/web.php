3cc56e19a7dced3b543b763974f7fd6cd23880f8
cp6 <cp6@users.noreply.github.com>
2023-07-29 23:28:27 +1000

    Moved server usage get route to be at /servers/{server}/usage/get instead of /servers/{server}/usage/

--- routes/web.php
+++ routes/web.php
@@ -33,8 +33,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/servers', [ServerController::class, 'store'])->name('server.store');
     Route::get('/servers/{server}', [ServerController::class, 'show'])->name('server.show');
     Route::get('/servers/{server}/get-information', [ServerController::class, 'getInformation'])->name('server.get-information');
-    Route::get('/servers/{server}/usage', [ServerController::class, 'getUsage'])->name('server.usage');
-    Route::get('/servers/{server}/usage/all', [ServerController::class, 'allUsage'])->name('server.usage.all');
+    Route::get('/servers/{server}/usage/', [ServerController::class, 'allUsage'])->name('server.usage.all');
+    Route::get('/servers/{server}/usage/get', [ServerController::class, 'getUsage'])->name('server.usage');
     Route::get('/servers/{server}/usage/ram', [ServerController::class, 'ramUsage'])->name('server.usage.ram');
     Route::get('/servers/{server}/usage/cpu', [ServerController::class, 'cpuUsage'])->name('server.usage.cpu');
     Route::get('/servers/{server}/usage/disk', [ServerController::class, 'diskUsage'])->name('server.usage.disk');
