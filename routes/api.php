9a61e07545feb0c3006eba77b9dd82ff50c08a9b
cp6 <cp6@users.noreply.github.com>
2023-07-30 13:13:57 +1000

    Added server usage get POST route to API controller

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,7 @@ Route::middleware(['auth:api', 'allowedIpApi'])->group(function () {
     Route::get('/servers/{server}/pings', [ApiController::class, 'serversPings'])->name('api.server.pings');
     Route::get('/servers/{server}/commands', [ApiController::class, 'serversCommands'])->name('api.server.commands');
     Route::get('/servers/{server}/connections', [ApiController::class, 'serversConnections'])->name('api.server.connections');
+    Route::post('/servers/{server}/usage', [ApiController::class, 'serversUsageGet'])->name('api.server.usage.get');
     Route::get('/servers/{server}/usage/all', [ApiController::class, 'serversUsageAll'])->name('api.server.usage.all');
     Route::get('/servers/{server}/usage/cpu', [ApiController::class, 'serversUsageCpu'])->name('api.server.usage.cpu');
     Route::get('/servers/{server}/usage/ram', [ApiController::class, 'serversUsageRam'])->name('api.server.usage.ram');
