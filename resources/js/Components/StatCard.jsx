331decfa745578decc0281ec1a972ecc10a532f0
cp6 <cp6@users.noreply.github.com>
2023-08-01 17:51:00 +1000

    Updated StatCard.jsx component link not covering whole card

--- resources/js/Components/StatCard.jsx
+++ resources/js/Components/StatCard.jsx
@@ -1,10 +1,12 @@
-export default function StatCard({ route, text = 'text', value = 123}) {
+export default function StatCard({route, text = 'text', value = 123}) {
     return (
         <div className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-700">
+            <a href={route} className="" tabIndex="0" role="link" title={'Go to ' + text}>
                 <div className="py-5 text-center">
-                    <a href={route} className="block text-2xl font-bold text-gray-800 dark:text-white" tabIndex="0" role="link">{value}</a>
+                    <p className="block text-2xl font-bold text-gray-800 dark:text-white">{value}</p>
                     <span className="text-sm text-gray-700 dark:text-gray-200">{text}</span>
                 </div>
+            </a>
         </div>
     );
 }
