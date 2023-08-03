5532b92e7ac36f1b8a90064ebef9464d90c2c02e
cp6 <cp6@users.noreply.github.com>
2023-08-03 22:20:41 +1000

    Created routes for saving command outputs to pdf

--- routes/web.php
+++ routes/web.php
@@ -95,6 +95,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/outputs/', [CommandOutputController::class, 'index'])->name('outputs.index');
     Route::get('/outputs/server/{server}', [CommandOutputController::class, 'showServer'])->name('outputs.show.server');
     Route::get('/outputs/command/{command}', [CommandOutputController::class, 'showCommand'])->name('outputs.show.command');
+    Route::get('/outputs/{commandOutput}/full-pdf', [CommandOutputController::class, 'downloadFullPdf'])->name('outputs.full-pdf');
+    Route::get('/outputs/{commandOutput}/simple-pdf', [CommandOutputController::class, 'downloadSimplePdf'])->name('outputs.simple-pdf');
 
     Route::get('/logs', [ActionLogController::class, 'index'])->name('log.index');
     Route::get('/logs/{actionLog}', [ActionLogController::class, 'show'])->name('log.show');
