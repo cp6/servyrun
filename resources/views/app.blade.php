089637a7728b37d996b57e6f5b84672eaf674349
cp6 <cp6@users.noreply.github.com>
2023-08-08 22:06:14 +1000

    Removed unneeded blade files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title inertia>{{ config('app.name', 'surcuri') }}</title>
+        <title inertia>{{ config('app.name', 'Servyrun') }}</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @routes
         @viteReactRefresh
