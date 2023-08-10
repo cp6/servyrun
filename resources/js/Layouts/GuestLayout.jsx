a391a478b00157edac1af586fde57414f138141c
cp6 <cp6@users.noreply.github.com>
2023-08-10 17:59:16 +1000

    Removed unneeded div from GuestLayout.jsx

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -4,12 +4,9 @@ import { Link } from '@inertiajs/inertia-react';
 export default function Guest({ children, wide = false }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-800">
-            <div>
                 <Link href="/">
                     <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
                 </Link>
-            </div>
-
             <div className={"w-full mt-6 " + (wide ? 'sm:max-w-5xl': 'sm:max-w-md')+ " px-6 py-4 bg-white dark:bg-gray-700 shadow-md overflow-hidden sm:rounded-lg"}>
                 {children}
             </div>
