6a83b49e6159d1382b489d190315ea9698fba520
cp6 <cp6@users.noreply.github.com>
2023-08-14 17:57:52 +1000

    Updated ServerCardSpecs.jsx values to be centered

--- resources/js/Components/ServerCardSpecs.jsx
+++ resources/js/Components/ServerCardSpecs.jsx
@@ -6,11 +6,11 @@ export default function ServerCardSpecs({resource}) {
         <dl className="flex items-center space-x-6 mt-2">
             <div>
                 <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">CPU</dt>
-                <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{resource.cpu_cores ?? '-'}</dd>
+                <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{resource.cpu_cores ?? '-'}</dd>
             </div>
             <div>
                 <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">Ghz</dt>
-                <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{numberFormat(resource.cpu_freq,3) ?? '-'}</dd>
+                <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{numberFormat(resource.cpu_freq,3) ?? '-'}</dd>
             </div>
             <div>
                 {
@@ -19,14 +19,14 @@ export default function ServerCardSpecs({resource}) {
                             return (
                                 <>
                                     <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">Disk TB</dt>
-                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{numberFormat(resource.disk_tb,3)}</dd>
+                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{numberFormat(resource.disk_tb,3)}</dd>
                                 </>
                             )
                         } else {
                             return (
                                 <>
                                     <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">Disk GB</dt>
-                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{(resource.disk_gb === null) ? '-' : numberFormat(resource.disk_gb,3)}</dd>
+                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{(resource.disk_gb === null) ? '-' : numberFormat(resource.disk_gb,3)}</dd>
                                 </>
                             )
                         }
@@ -40,14 +40,14 @@ export default function ServerCardSpecs({resource}) {
                             return (
                                 <>
                                     <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">RAM GB</dt>
-                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{numberFormat(resource.ram_gb,3)}</dd>
+                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{numberFormat(resource.ram_gb,3)}</dd>
                                 </>
                             )
                         } else {
                             return (
                                 <>
                                     <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">RAM MB</dt>
-                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{(resource.ram_mb === null) ? '-' : numberFormat(resource.ram_mb,3)}</dd>
+                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{(resource.ram_mb === null) ? '-' : numberFormat(resource.ram_mb,3)}</dd>
                                 </>
                             )
                         }
@@ -61,7 +61,7 @@ export default function ServerCardSpecs({resource}) {
                             return (
                                 <>
                                     <dt className="mb-2 font-light leading-none text-gray-900 dark:text-gray-300">Price {resource.currency}</dt>
-                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white">{resource.price}</dd>
+                                    <dd className="mb-4 font-semibold text-gray-600 sm:mb-5 dark:text-white text-center">{resource.price}</dd>
                                 </>
                             )
                         }
