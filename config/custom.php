ce551e90c2bd67b81a52d64ee91053bab076bd8c
cp6 <cp6@users.noreply.github.com>
2023-09-01 17:56:08 +1000

    Added config/env option to point to a custom favicon .ico

--- config/custom.php
+++ config/custom.php
@@ -4,5 +4,6 @@ return [
     'maxAccounts' => env('MAX_ACCOUNTS', 1),
     'maxServersPerAccount' => env('MAX_SERVERS_PER_ACCOUNT', 20),
     'maxCommandsPerAccount' => env('MAX_COMMANDS_PER_ACCOUNT', 30),
-    'canPing' => env('CAN_PING', true)
+    'canPing' => env('CAN_PING', true),
+    'favicon' => env('FAVICON', 'favicon.ico'),
 ];
