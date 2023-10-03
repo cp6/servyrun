3676bf9eec8386168fd45fef77cb5f5af7ce0a95
cp6 <cp6@users.noreply.github.com>
2023-10-03 22:58:34 +1100

    Added user_id column to locations table

--- database/migrations/2023_01_06_141931_create_locations_table.php
+++ database/migrations/2023_01_06_141931_create_locations_table.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->char('user_id')->nullable()->default(null);
             $table->string('name')->unique();
             $table->string('country')->default(null)->nullable();
             $table->float('lat')->default(null)->nullable();
             $table->float('lon')->default(null)->nullable();
             $table->timestamps();
+            $table->unique(['user_id', 'name']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
