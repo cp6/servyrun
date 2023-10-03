876d2614d926fa289d712d26fd306ed14b43758c
cp6 <cp6@users.noreply.github.com>
2023-10-03 22:59:58 +1100

    Added LocationController.php

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Location;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Location $location)
+    {
+        //
+    }
+
+    public function edit(Location $location)
+    {
+        //
+    }
+
+    public function update(Request $request, Location $location)
+    {
+        //
+    }
+
+    public function destroy(Location $location)
+    {
+        //
+    }
+}
