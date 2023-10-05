184360d4438045fa641ac73b3846b82f0f02b73e
cp6 <cp6@users.noreply.github.com>
2023-10-05 22:25:49 +1100

    Continuation of favicon fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name', 'Servyrun') }}</title>
-        <link rel="icon" type="image/x-icon" href="{{config('custom.favicon', 'favicon.ico')}}">
+        <link rel="icon" type="image/x-icon" href="{{asset(config('custom.favicon', 'favicon.ico'))}}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @routes
         @viteReactRefresh
