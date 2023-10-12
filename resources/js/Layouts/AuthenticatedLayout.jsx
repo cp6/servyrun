6a9eaff1754e18af6b58447dd59d696cb87b8461
cp6 <cp6@users.noreply.github.com>
2023-10-13 10:06:09 +1100

    Updated horizontal padding on header text. Removed it from large screens and reduced it for mobile screens

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -242,7 +242,7 @@ export default function Authenticated({auth, header, children}) {
 
             {header && (
                 <header className="bg-gray-50 dark:bg-gray-700 shadow">
-                    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
+                    <div className="max-w-7xl mx-auto py-6 px-2">{header}</div>
                 </header>
             )}
 
