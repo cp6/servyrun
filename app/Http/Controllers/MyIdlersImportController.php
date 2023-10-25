7767ec05689d9ee28f05e3df4a7581fbca83c616
cp6 <cp6@users.noreply.github.com>
2023-10-25 23:27:50 +1100

    Added MyIdlersImportController.php for importing My idlers server data

---
+++ app/Http/Controllers/MyIdlersImportController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Inertia\Inertia;
+
+class MyIdlersImportController extends Controller
+{
+    public function index(): \Inertia\Response
+    {
+        return Inertia::render('', [
+            'alert' => \Session::get('alert')
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'url' => 'url|required',
+            'api_token' => 'string|required|size:60'
+        ]);
+
+        $call = Http::withHeaders(['Authorization' => 'Bearer ' . $request->api_token, 'Accept' => 'application/json'])->get($request->url);
+
+        if ($call->status() === 200) {
+
+            $servers = $call->json();
+
+            foreach ($servers as $server) {
+
+            }
+
+        }
+
+
+    }
+
+}
