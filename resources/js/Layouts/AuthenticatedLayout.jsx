427c0efe174b9af025c1b5033f5a9943fc98fafa
cp6 <cp6@users.noreply.github.com>
2023-10-28 00:20:59 +1100

    Added my idlers import link to navbar dropdown menu

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -132,6 +132,7 @@ export default function Authenticated({auth, header, children}) {
                                     </Dropdown.Trigger>
                                     <Dropdown.Content>
                                         <Dropdown.Link href={route('location.index')}>Locations</Dropdown.Link>
+                                        <Dropdown.Link href={route('import.index')}>My Idlers import</Dropdown.Link>
                                         <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
                                         <Dropdown.Link href={route('logout')} method="post" as="button">
                                             Log Out
@@ -231,6 +232,7 @@ export default function Authenticated({auth, header, children}) {
 
                         <div className="mt-3 space-y-1">
                             <ResponsiveNavLink href={route('location.index')}>Locations</ResponsiveNavLink>
+                            <ResponsiveNavLink href={route('import.index')}>My Idlers import</ResponsiveNavLink>
                             <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
                             <ResponsiveNavLink method="post" href={route('logout')} as="button">
                                 Log Out
